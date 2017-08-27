167592c362f5e3683b4aed5277f2768dc6a8c456
Juan Jose Lopez Jaimez <jj.lopezjaimez@gmail.com>
2017-08-27 12:06:26 -0500

    Add animation and speech feature

--- app/src/main/java/dramaqueens/queen/MainActivity.java
+++ app/src/main/java/dramaqueens/queen/MainActivity.java
@@ -7,42 +7,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Locale;
-
-import com.google.api.GoogleAPI;
-import com.google.api.GoogleAPIException;
-import com.google.api.translate.Language;
-import com.google.api.translate.Translate;
 
 public class MainActivity extends AppCompatActivity {
 
-    protected TextView transliterated_speech;
-    protected Button microphone;
+
     protected ImageView animation_view;
     protected Animation animation;
-    protected final int REQ_SPEECH = 100;
     @Override
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        microphone = (Button) findViewById(R.id.Microphone);
-        transliterated_speech = (TextView) findViewById(R.id.SpeechOut);
-        String translatedText = GTranslate.translatedText("Hello world", Language.SPANISH, Language.ENGLISH);
 
-        System.out.println(translatedText);
-
-    microphone.setOnClickListener(new View.OnClickListener() {
-        @Override
-            public void onClick(View view) {
-                askSpeech();
-            }
-        });
         animation_view = (ImageView) findViewById(R.id.LoadingImageView);
         int[] frames = getResourceId("beea", 45);
         final Intent intent = new Intent(this, beeText_Main.class);
@@ -62,29 +42,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return output;
     }
-    private void askSpeech() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "es_MX");
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Bzzzzzzz");
-        try {
-            startActivityForResult(intent, REQ_SPEECH);
-        } catch(ActivityNotFoundException a) {
 
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch(requestCode) {
-            case REQ_SPEECH:{
-                if(resultCode == RESULT_OK && data != null) {
-                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    transliterated_speech.setText(result.get(0));
-                }
-            }
-
-        }
-    }
 
 }
--- app/src/main/java/dramaqueens/queen/beeText_Main.java
+++ app/src/main/java/dramaqueens/queen/beeText_Main.java
@@ -3,6 +3,7 @@ package dramaqueens.queen;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
@@ -12,20 +13,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.CompoundButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import com.google.api.translate.Language;
+
 import java.util.ArrayList;
 
-public class beeText_Main extends AppCompatActivity implements RecognitionListener{
+public class beeText_Main extends AppCompatActivity implements RecognitionListener, AdapterView.OnItemSelectedListener {
     private String LOG_TAG = "VoiceRecognition";
     private SpeechRecognizer speech = null;
     private Intent recognizerIntent;
     private TextView capturedText;
     private ToggleButton toggleButton;
-
-
+    protected Chronometer chronos;
+    Language origin_language = Language.SPANISH;
+    Language target_language = Language.SPANISH;
+    private String original_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,22 +47,47 @@ public class beeText_Main extends AppCompatActivity implements RecognitionListen
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "es_MX");
         toggleButton = (ToggleButton) findViewById(R.id.micButton);
-
+        chronos = (Chronometer) findViewById(R.id.timeRecorded);
         toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
             @Override
             public void onCheckedChanged(CompoundButton buttonView,
                                          boolean isChecked) {
                 if (isChecked) {
-
+                    toggleButton.setBackgroundResource(R.drawable.rsz_micpress);
                     speech.startListening(recognizerIntent);
+                    chronos.setBase(SystemClock.elapsedRealtime());
+                    chronos.start();
                 } else {
-
-                    toggleButton.setBackgroundResource(R.drawable.mic0);
+                    chronos.stop();
+                    toggleButton.setBackgroundResource(R.drawable.rsz_mic);
                     speech.stopListening();
                 }
             }
         });
+
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.target_language);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.language_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(this);
+
+        Button translate = (Button) findViewById(R.id.translate_button);
+        translate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(capturedText.getText()!=""){
+                    String translated = GTranslate.translatedText(original_text, target_language, origin_language);
+                    capturedText.setText(translated);
+                }
+            }
+        });
+
     }
     private int[] getResourceId(String base_name, int number){
         int output[] = new int[number+1];
@@ -93,8 +128,8 @@ public class beeText_Main extends AppCompatActivity implements RecognitionListen
         String errorMessage = getErrorText(errorCode);
         Log.d(LOG_TAG, "FAILED " + errorMessage);
         capturedText.setText(errorMessage);
-
-        toggleButton.setChecked(false);
+        chronos.stop();
+        toggleButton.setBackgroundResource(R.drawable.rsz_mic);
     }
 
     @Override
@@ -105,6 +140,7 @@ public class beeText_Main extends AppCompatActivity implements RecognitionListen
 
 
         capturedText.setText(matches.get(0));
+        original_text = matches.get(0);
     }
 
     @Override
@@ -152,5 +188,33 @@ public class beeText_Main extends AppCompatActivity implements RecognitionListen
                 break;
         }
         return message;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        switch (i){
+            case 0:
+                target_language = Language.SPANISH;
+                break;
+            case 1:
+                target_language = Language.ENGLISH;
+                break;
+            case 2:
+                target_language = Language.FRENCH;
+                break;
+            case 3:
+                target_language = Language.GERMAN;
+                break;
+            case 4:
+                target_language = Language.FINNISH;
+                break;
+
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
